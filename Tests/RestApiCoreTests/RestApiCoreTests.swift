fa4b8651e8a6ab226492ce2b82b7d1391357a1c9
Sergey Starukhin <star.s@me.com>
2020-02-01 18:10:06 +0100

    minor edits

--- Tests/RestApiCoreTests/RestApiCoreTests.swift
+++ Tests/RestApiCoreTests/RestApiCoreTests.swift
@@ -6,7 +6,7 @@ final class RestApiCoreTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(RestApiCore().text, "Hello, World!")
+        //XCTAssertEqual(RestApiCore().text, "Hello, World!")
     }
 
     static var allTests = [
