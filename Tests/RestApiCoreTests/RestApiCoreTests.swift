79c4531e5b7f5c515314dc4d1e9238c60bfc2490
Sergey Starukhin <sergey.starukhin@altarix.ru>
2020-02-19 21:33:22 +0100

    add some tests

--- Tests/RestApiCoreTests/RestApiCoreTests.swift
+++ Tests/RestApiCoreTests/RestApiCoreTests.swift
@@ -9,7 +9,39 @@ final class RestApiCoreTests: XCTestCase {
         //XCTAssertEqual(RestApiCore().text, "Hello, World!")
     }
 
+    struct Response<T: Decodable>: Decodable {
+        var result: T
+    }
+    
+    @discardableResult
+    func decodeVoidResponse(from json: String) throws -> Response<VoidResponse> {
+        guard let data = json.data(using: .utf8) else {
+            fatalError("Wrong json: \(json)")
+        }
+        return try ApiMethod("").makeJsonDecoder().decode(Response<VoidResponse>.self, from: data)
+    }
+    
+    func testNullFromText() {
+        let json = "{\"result\":\"OK\"}"
+        XCTAssertNoThrow(try decodeVoidResponse(from: json))
+    }
+    
+    func testNullFromInt() {
+        let json = "{\"result\": 42}"
+        XCTAssertNoThrow(try decodeVoidResponse(from: json))
+    }
+    
+    func testNullFromNull() {
+        let json = "{\"result\": null}"
+        XCTAssertNoThrow(try decodeVoidResponse(from: json))
+    }
+    
+    func testEmptyNull() {
+        let json = "{}"
+        XCTAssertThrowsError(try decodeVoidResponse(from: json))
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testExample), ("testNull", testNullFromText), ("testEmptyNull", testEmptyNull)
     ]
 }
